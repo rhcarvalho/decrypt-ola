4158f6235fc16414df270c5f5e065a3d4466ffcc
Aleksandra Kwoka <adkwoka@gmail.com>
2013-04-07 11:12:36 -0300

    Fix Bacon's cipher

--- binary/binary.go
+++ binary/binary.go
@@ -6,16 +6,30 @@ import (
 	"strings"
 )
 
-func main() {
-	m := "BAABAAABAAAAAAAABABBABBAB"
-	m = strings.Map(func(r rune) rune {
+// http://en.wikipedia.org/wiki/Bacon's_cipher
+
+func abCodeToChar(abCode string) string {
+	binaryCode := strings.Map(func(r rune) rune {
 		if r == 'A' {
 			return '0'
 		}
 		return '1'
-	}, m)
-	for i := 0; i < len(m); i += 5 {
-		d, _ := strconv.ParseInt(m[i:i+5], 2, 0)
-		fmt.Print(string('A' + d))
+	}, abCode)
+	d, _ := strconv.ParseInt(binaryCode, 2, 0)
+	char := 'A' + d
+	if char > 'I' {
+		char += 1
 	}
+	if char > 'U' {
+		char += 1
+	}
+	return string(char)
+}
+
+func main() {
+	m := "BAABAAABAAAAAAAABABBABBAB"
+	for i := 0; i < len(m); i += 5 {
+		fmt.Print(abCodeToChar(m[i:i+5]))
+	}
+	fmt.Println()
 }
